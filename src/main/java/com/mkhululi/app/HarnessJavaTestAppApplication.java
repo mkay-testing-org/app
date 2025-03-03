a6d99751648b23cfdf0cb7ed3c6ce8ee94c7c1f4
Mkhululi Mtshemla <mkhululi.mtshemla@standardbank.co.za>
2025-03-03 13:12:50 +0200

    init

---
+++ src/main/java/com/mkhululi/app/HarnessJavaTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.mkhululi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarnessJavaTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarnessJavaTestAppApplication.class, args);
+	}
+
+}
